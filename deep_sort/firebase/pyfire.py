acf0064b5df2806aa808c9ca641ede0d7447909b
manupillai308 <manupillai308@gmail.com>
2019-03-03 07:24:34 +0530

    version-2.1

--- deep_sort/firebase/pyfire.py
+++ deep_sort/firebase/pyfire.py
@@ -27,18 +27,25 @@ storage = firebase.storage()
 
 def put_data(name, first_seen, last_seen):		
 	all_users = db.child("Users").get()
+	users = {}
 	person = {}
 	for user in all_users.each():
 		if user.val().get("nothing") is not None:
 			break
+		users[user.key()] = user.val()
 		person[user.val()['name']] = user.key()
 	person_name = glob.glob('./images/%s*' % name.lower())[0]
-	person_id = person.get(name)
+	person_id = person.get(name.lower())
 	storage.child("profile_images/%s" % person_name.split('/')[-1]).put(person_name)
 	url = storage.child("profile_images/%s" % person_name.split('/')[-1]).get_url(token=None)
+	tracks = {}
 	if person_id is None:
 		person_id = len(person) + 1
+		tracks["0%d" % 1] = {"first_seen":first_seen, "last_seen":last_seen, "image": url }
 	else:
 		person_id = int(person_id)
-	data = {"Users/0%d"% person_id:{"image":url, "first":first_seen, "name":name.lower(), "status":last_seen}}
+		tracks = users["0%d" % person_id]["tracks"]
+		track_no = len(tracks) + 1
+		tracks["0%d" % track_no] = {"first_seen":first_seen, "last_seen":last_seen, "image": url }
+	data = {"Users/0%d"% person_id:{"image":url, "first":first_seen, "name":name.lower(), "status":last_seen, "tracks":tracks}}
 	db.update(data)
